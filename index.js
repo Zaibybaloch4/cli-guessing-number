984bf917325de638207bf3a0f385956ceca69297
Zaibybaloch4 <zaibybaloch28@gmail.com>
2024-03-25 02:59:29 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+#!  /usr/bin/env node
+import inquirer from "inquirer";
+const randomNumber = Math.floor(Math.random() * 7 + 1);
+const answers = await inquirer.prompt([
+    {
+        name: "userGuessedNumber",
+        type: "number",
+        message: "Please guess a number",
+    },
+]);
+console.log(answers);
+//conditional statement
+if (answers.userGuessedNumber === randomNumber) {
+    console.log("CONGRATULATION  YOUR GUESSED NUMBER IS CORRECT.");
+}
+else {
+    console.log("YOU GUESSED A WRONG NUMBER.");
+}
